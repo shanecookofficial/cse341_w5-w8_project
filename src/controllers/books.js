73289cf23c4cb17623867a92ac8673d8070ea4fc
shanecookofficial <shanecookofficial@gmail.com>
2024-04-07 16:25:58 -0500

    Added a get route that gets all books

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,6 +1,15 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
+const getAll = async (req, res, next) => {
+  const result = await mongodb.getDb().db('librarydb').collection('books').find();
+
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
+};
+
 const addBook = async (req, res) => {
   const contact = {
     title: req.body.title,
@@ -20,5 +29,6 @@ const addBook = async (req, res) => {
 
 
 module.exports = {
+  getAll,
   addBook
 };
--- src/routes/books.js
+++ src/routes/books.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/books');
 
+router.get('/', bookController.getAll);
 router.post('/', bookController.addBook);
 
 module.exports = router;
